0f3e8df750cdb819213ecb255a69ff7a02ae6edb
Christina Bergstrom <bergstromcbb@gmail.com>
2016-11-21 16:16:10 -0500

    updated lab

---
+++ script.js
@@ -0,0 +1,53 @@
+var app = angular.module("catdogApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+
+    $routeProvider.when("/cat", {
+        templateUrl: "cat.html",
+        controller: "picController"
+    });
+
+    $routeProvider.when("/dog", {
+        templateUrl: "dog.html",
+        controller: "picController"
+    });
+
+    $routeProvider.when("/racoon", {
+            templateUrl: "racoon.html",
+            controller: "picController"
+        });
+
+    $routeProvider.when("/elephant", {
+            templateUrl: "elephant.html",
+            controller: "picController"
+        });
+
+    $routeProvider.when("/horse", {
+            templateUrl: "horse.html",
+            controller: "picController"
+        });
+
+    $routeProvider.when("/sheep", {
+            templateUrl: "sheep.html",
+            controller: "picController"
+        });
+
+    $routeProvider.when("/squirrel", {
+            templateUrl: "squirrel.html",
+            controller: "picController"
+        });
+
+    $routeProvider.otherwise({
+        template: "You messed it all up!!!!!"
+    });
+
+
+});
+
+
+
+app.controller("picController", function() {
+  
+});
+
+
